f5e27ab1d3d1245e6e957e6db8c8d8f87e640221
VikramBaskar <vikrambaskar796@gmail.com>
2025-03-10 12:23:40 +0530

    adding new folder and file in javascript

---
+++ array.js
@@ -0,0 +1,8 @@
+
+let flavors = ["vanila","butterscortch","lavandar","chocolate"]
+
+// console.log(flavors[0])
+// console.log(flavors[1])
+// console.log(flavors[2])
+// console.log(flavors[3])
+console.log(flavors[flavors.length-1]);
