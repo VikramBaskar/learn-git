372b389eb908dd819e62256a73b5e8ac233b9eee
VikramBaskar <vikrambaskar796@gmail.com>
2025-03-11 15:45:44 +0530

    Adding the file in Javascript branch

---
+++ for-inloop.js
@@ -0,0 +1,35 @@
+// let person1 = {
+//     Uname :"BenTen",
+//     Hobbies :["cricket","FootBall",'Caramboard',"basketball"],
+//     familydetails : {
+//         totalmember: 5,
+//     siblings :["a","b","c"]
+// },
+
+//     Walk(){
+//         console.log("Im going to home");
+        
+//     }
+// }
+
+// for(let key in person1){
+//     console.log(person1[key])
+// }
+
+person2 ={
+    Uname :"badass",
+    age :23,
+    hobbies :["Kill","briyani Making","chainSmokker"],
+    work(){
+       console.log("Going to kill someone")
+    },
+    familydetails :{
+        totalmember :6,
+        othermember :["uncle","grandFather"]
+    }
+
+}
+for(let key in person2){
+    console.log(key);
+    
+}
--- forOf-loop.js
+++ forOf-loop.js
@@ -1,6 +1,31 @@
-let arr =[10,100,1000,10000]
+// let arr =[10,100,1000,10000]
 
-for(let iterator of arr){
-    console.log(iterator);
+// for(let i of arr){
+//     console.log(i);   
+// }
+
+// let str = "Javascript"
+// for(let char of str){
+//     console.log(char)
+// }
+
+function* genfunc(){
+    yield 'one'
+    yield 'two'
+    yield 'three'
+    yield 'four'
+}
+let iterator = genfunc()
+
+for(let val of iterator){
+    console.log(val);
     
 }
+
+// let obj ={
+//     id:1
+// }
+// for(let v of obj){
+//     console.log(v);
+    
+// }
