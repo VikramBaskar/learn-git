072de4d50dcd9220703a995e332a9a091b1770f4
VikramBaskar <vikrambaskar796@gmail.com>
2025-03-11 11:32:36 +0530

    changes in object file

--- object.js
+++ object.js
@@ -26,3 +26,23 @@ let userprofile ={
 // console.log(vechile["fueltype"]);
 // console.log(vechile["seater type"][0]);
 
+//shorthand assigned property
+//dynamic property
+
+let Uname = "Vikraman"
+let age = 26
+
+// let person1 = {
+//     Uname,
+//     age
+// }
+// console.log(person1.Uname,person1.age);
+
+let dynamicprop= "empid"
+let person2 = {
+    Uname,age,
+    ["dynamicprop"] :'IFS234'
+}
+console.log(person2);
+console.log(person2.Uname,person2.age,person2['dynamicprop']);
+
