e9f11d8ccc068eb0c1644821045a0c8cfdbf431b
VikramBaskar <vikrambaskar796@gmail.com>
2025-03-12 10:45:24 +0530

    modifying the for-inloop.js

--- for-inloop.js
+++ for-inloop.js
@@ -16,20 +16,49 @@
 //     console.log(person1[key])
 // }
 
-person2 ={
-    Uname :"badass",
-    age :23,
-    hobbies :["Kill","briyani Making","chainSmokker"],
-    work(){
-       console.log("Going to kill someone")
-    },
-    familydetails :{
-        totalmember :6,
-        othermember :["uncle","grandFather"]
-    }
+// person2 ={
+//     Uname :"badass",
+//     age :23,
+//     hobbies :["Kill","briyani Making","chainSmokker"],
+//     work(){
+//        console.log("Going to kill someone")
+//     },
+//     familydetails :{
+//         totalmember :6,
+//         othermember :["uncle","grandFather"]
+//     }
 
+// }
+// for(let key in person2){
+//     console.log(key);
+    
+// }
+
+person3={
+    Hname:"Mohan",
+    eyes:"Black",
+    charcter:"Good",
+    Hobbies:["Instragram","clash-of-clanes","gossips"],
+    fun(){
+        console.log("He is a funney guy");
+        
+    }
 }
-for(let key in person2){
-    console.log(key);
+
+for(let hai in person3){
+    //console.log(hai);
+    console.log(person3[hai]);
+    // console.log(person3.hai);
+}
+
+let arr =[11,12,13,14,15];
+for(let key in arr){
+    console.log(arr[key]);
+    
+}
+
+let str = "ECMA Script"
+for(let key in str){
+    console.log(str[key]+1);
     
 }
